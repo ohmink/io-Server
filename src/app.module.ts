f700f88fca89b241151b25e48882714ca7fb35f8
ohmink <kkop5056@naver.com>
2021-06-19 22:41:23 +0900

    user, auth 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-// import { CatsModule } from './cats/cats.module';
 import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:dhals02@localhost:27017/'),
     PostsModule,
+    UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
