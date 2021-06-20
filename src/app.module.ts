f5da96b74177fbe4a73e842b0d85ab88c41b01c5
ohmink <kkop5056@naver.com>
2021-06-21 00:12:42 +0900

    env 라인 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env.MONGODB_URL ||
-        'mongodb+srv://ohmink:dhals02@cluster0.l8qex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      'mongodb+srv://ohmink:dhals02@cluster0.l8qex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
       },
