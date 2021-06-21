428f2951f6596540749723866a70e6c2bffe2472
ohmink <kkop5056@naver.com>
2021-06-21 15:45:22 +0900

    heroku sleep 방지

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user-dto';
@@ -7,7 +7,10 @@ import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private httpService: HttpService,
+  ) {}
 
   async findOne(username: string): Promise<User> {
     return await this.userModel.findOne({ username: username }).exec();
@@ -18,8 +21,9 @@ export class UserService {
     return await newMaster.save();
   }
 
-  @Cron('0 */29 0-12 * * *')
-  wakeUp() {
+  @Cron('0 */20 23,0-14 * * *')
+  async wakeUp() {
     console.log('Wake up with Cron');
+    this.httpService.get('https://ohmink-blog-server.herokuapp.com');
   }
 }
