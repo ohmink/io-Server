7bd9ef3ea981937c8f827e5a52212dbdb9ff31f7
ohmink <kkop5056@naver.com>
2021-06-30 15:58:38 +0900

    remove @nestjs/schedule

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,12 @@
 import { Model } from 'mongoose';
-import { Injectable, HttpService } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user-dto';
-import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-    private httpService: HttpService,
-  ) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async findOne(username: string): Promise<User> {
     return await this.userModel.findOne({ username: username }).exec();
@@ -19,14 +15,5 @@ export class UserService {
   async signUp(user: CreateUserDto) {
     const newMaster = new this.userModel(user);
     return await newMaster.save();
-  }
-
-  @Cron('0 */20 23,0-14 * * *')
-  async wakeUp() {
-    const res = await this.httpService
-      .get('https://ohmink-blog-server.herokuapp.com/posts/getAll')
-      .toPromise();
-
-    console.log('Wake up with Cron, res status is ', res.status);
   }
 }
