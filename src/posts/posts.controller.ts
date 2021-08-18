ecb76be885902477aa907a34fa314155266bd90d
ohmink <kkop5056@naver.com>
2021-08-18 20:10:57 +0900

    삭제 수정 함수 버그 수정 및 주석 삭제

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -18,33 +18,33 @@ import { UpdatePostsDto } from './dto/update-posts.dto';
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @Get('getAll') // 모든 게시글
+  @Get('getAll')
   getAll() {
     const answer = this.postsService.getAll();
     if (!answer) throw new NotFoundException('Not founded!');
     return answer;
   }
 
-  @Get('getDetail/:id') // 게시글 상세
+  @Get('getDetail/:id')
   getDetail(@Param() postsId: { id: string }) {
     return this.postsService.getDetail(postsId.id);
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Post('create') // 게시글 생성
+  @Post('create')
   create(@Body() postsData: CreatePostsDto) {
     return this.postsService.create(postsData);
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Delete('delete/:id') // 게시글 삭제
+  @Delete('delete/:id')
   delete(@Param('id') postsId: string) {
-    return this.postsService.delete(postsId);
+    return this.postsService.delete(postsId.substring(1));
   }
 
   @UseGuards(AuthGuard('jwt'))
-  @Patch('patch/:id') // 게시글 수정
+  @Patch('patch/:id')
   update(@Param('id') postsId: string, @Body() updateData: UpdatePostsDto) {
-    return this.postsService.update(postsId, updateData);
+    return this.postsService.update(postsId.substring(1), updateData);
   }
 }
